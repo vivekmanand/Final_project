c3fb91743c7d594ab2655126059afd4d5823b592
Vijay P R <www.vijaywyd@gmail.com>
2016-11-16 12:00:32 +0530

    Update Hello.c

--- src/Hello.c
+++ src/Hello.c
@@ -3,5 +3,6 @@
 int main() {
   printf("Hello World\n");
   printf("Added some lines directly from master\n");
+  printf("Created new branch in local called test_merge\n");
   printf("Another line in local branch to commit to master (11:55)\n");
 }
